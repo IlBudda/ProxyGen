10658c539bdbf810f6f4d478731ffd32ee9d4a13
root <root@localhost.localdomain>
2020-04-09 07:27:31 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,81 @@
+<h1 align="center">Proxygen
+
+</h1>
+<p align="center">An open-source Proxy Generator.</p><br>
+
+
+###############  Coded by      : Tuhin Bose ##############<br>
+########### Inspired by        : Charlie:The Hacker ##############<br>
+##############  Instagram id   : www.instagram.com/tuhin1729 ##############<br>
+##############  Github         : www.github.com/tuhin1729 ##############<br>
+############## YouTube Channel : https://bit.ly/TuhinTheHacker ##############<br>
+############## Dedicated to    : Diya Saha ##############<br>
+
+
+
+#### By Changing the name, You won't be a script developer...
+
+## Note:
+
+- The script requires working network connection to work.
+- This script is only for educational purposes.
+- **Developer is not responsible for the misuse of MailDiya.**
+<br>
+
+## Features:
+
+- US Proxy Generator
+- Uk Proxy Generator
+- SSL Proxy Generator
+- Anonymous Proxy Generator
+- Socks Proxy Generator
+- Easy to use.
+
+## Usage:
+
+Run these commands to run ProxyGen
+
+### > For Termux:
+
+**Notice:** 
+
+git installation methods are not universal and do differ between distributions,
+so, installing git as per instructions below may not work.
+Please check out how to install `git` for your Linux distribution.
+Commands below provide instructions for Debian-based systems.
+
+To use the scanner type the following commands in Termux:
+```
+pkg install git
+pkg install python2
+git clone https://github.com/tuhin1729/ProxyGen.git
+cd ProxyGen
+pip2 install -r requirements.txt
+python2 proxygen.py
+```
+
+### > For Linux:
+
+**Notice:** 
+
+To use the scanner type the following commands in Linux terminal:
+```
+git clone https://github.com/tuhin1729/ProxyGen.git
+cd ProxyGen
+pip install -r requirements.txt
+python2 proxygen.py
+```
+
+
+
+# CONTACT ME:
+
+Feel Free To Open An Issue...
+
+```
+                 Instagram id: @tuhin1729
+       YouTube Channel : https://bit.ly/TuhinTheHacker
+```
+
+
+
---
+++ proxygen.py
@@ -0,0 +1,173 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+import time
+import random
+import sys
+os.system("apt install toilet")
+color1=["\033[1;31;40m","\033[1;32;40m","\033[1;33;40m","\033[1;34;40m","\033[1;35;40m","\033[1;36;40m"]
+def color():
+ return str(random.choice(color1))
+def generate(url,count):
+ r=requests.get(url)
+ soup=BeautifulSoup(r.content,'html.parser')
+ list = soup.find(class_='table table-striped table-bordered')
+ list1 = list.findAll('td')
+ iplist=[]
+ portlist=[]
+ codelist=[]
+ countrylist=[]
+ anonymitylist=[]
+ googlelist=[]
+ httpslist=[]
+ last_checklist=[]
+ finallist=[]
+ j=0
+ while(j<(8*count)):
+  a=str(list1[j].contents[0])
+  iplist.append(a)
+  j=j+8
+ j=1
+ while(j<(8*count)):
+  a=str(list1[j].contents[0])
+  portlist.append(a)
+  j=j+8
+ j=2   
+ while(j<(8*count)):
+  a=str(list1[j].contents[0])
+  codelist.append(a)
+  j=j+8   
+ j=3   
+ while(j<(8*count)):
+  a=str(list1[j].contents[0])
+  countrylist.append(a)
+  j=j+8 
+ j=4   
+ while(j<(8*count)):
+  a=str(list1[j].contents[0])
+  anonymitylist.append(a)
+  j=j+8
+ j=5   
+ while(j<(8*count)):
+  a=str(list1[j].contents[0])
+  googlelist.append(a)
+  j=j+8  
+ j=6   
+ while(j<(8*count)):
+  a=str(list1[j].contents[0])
+  httpslist.append(a)
+  j=j+8  
+ j=7   
+ while(j<(8*count)):
+  a=str(list1[j].contents[0])
+  last_checklist.append(a)
+  j=j+8
+ for k in range (0,count):
+     print(color()+iplist[k]+":"+color()+portlist[k]+"\t"+color()+codelist[k]+"\t"+color()+countrylist[k]+"\t\t"+color()+anonymitylist[k]+color()+"\tGoogle:"+googlelist[k]+color()+"\thttps:"+httpslist[k]+color()+"\tLast checked "+last_checklist[k]+"\n")
+def socks(count):
+ r=requests.get("https://www.socks-proxy.net")
+ soup=BeautifulSoup(r.content,'html.parser')
+ list = soup.find(class_='table table-striped table-bordered')
+ list1 = list.findAll('td')
+ iplist=[]
+ portlist=[]
+ codelist=[]
+ countrylist=[]
+ versionlist=[]
+ anonymitylist=[]
+ httpslist=[]
+ last_checklist=[]
+ j=0
+ while(j<8*count):
+  a=str((list1[j].contents[0]))
+  iplist.append(a)
+  j=j+8
+ j=1
+ while(j<8*count):
+  a=str((list1[j].contents[0]))
+  portlist.append(a)
+  j=j+8
+ j=2
+ while(j<8*count):
+  a=str((list1[j].contents[0]))
+  codelist.append(a)
+  j=j+8
+ j=3
+ while(j<8*count):
+  a=str((list1[j].contents[0]))
+  countrylist.append(a)
+  j=j+8
+ j=4
+ while(j<8*count):
+  a=str((list1[j].contents[0]))
+  versionlist.append(a)
+  j=j+8
+ j=5
+ while(j<8*count):
+  a=str((list1[j].contents[0]))
+  anonymitylist.append(a)
+  j=j+8
+ j=6
+ while(j<8*count):
+  a=str((list1[j].contents[0]))
+  httpslist.append(a)
+  j=j+8
+ j=7
+ while(j<8*count):
+  a=str((list1[j].contents[0]))
+  last_checklist.append(a)
+  j=j+8
+
+ for k in range (0,count):
+  print(color()+iplist[k]+":"+color()+portlist[k]+"\t"+color()+codelist[k]+"\t"+color()+countrylist[k]+"\t"+color()+versionlist[k]+"\t"+color()+anonymitylist[k]+"\thttps:"+color()+httpslist[k]+"\tLast Checked :"+color()+last_checklist[k]+"\n")
+def banner():
+ os.system("clear")
+ os.system("toilet -fmono12 -F gay ProxyGen")
+ print("    \033[1;36;40m Code made by: \033[1;32;40m Tuhin Bose")
+ print("    \033[1;36;40m Inspired by : \033[1;32;40m Charlie:The Hacker")
+ print("    \033[1;36;40m Instagram id: \033[1;32;40m www.instagram.com/tuhin1729")
+ print("    \033[1;36;40m Github      : \033[1;32;40m www.github.com/tuhin1729")
+ print("    \033[1;36;40m YouTube     : \033[1;32;40m https://bit.ly/TuhinTheHacker")
+ print("    \033[1;36;40m Dedicated to: \033[1;34;40m Diya Saha")
+ print("    \033[1;31;40mIf you are using termux then zoom out your screen otherwise you won't understand anything.")
+ print("\n\n")
+
+banner()
+number=int(raw_input("\033[1;33;40mEnter the number of proxies you want to generate(Max:10) :"))
+if(number>10):
+ print("\033[1;31;40mEnter a number <=10. Quiting...")
+ time.sleep(1.5)
+ sys.exit()  
+banner()
+print("          \033[1;32;40m[1] \033[1;36;40mNEW PROXY")
+print("          \033[1;32;40m[2] \033[1;36;40mUS PROXY")
+print("          \033[1;32;40m[3] \033[1;36;40mUK PROXY")
+print("          \033[1;32;40m[4] \033[1;36;40mSSL PROXY")
+print("          \033[1;32;40m[5] \033[1;36;40mANONYMOUS PROXY")
+print("          \033[1;32;40m[6] \033[1;36;40mSOCKS PROXY")
+
+op=int(raw_input("Diya>>>"))
+banner()
+if(op==1):
+ generate("https://free-proxy-list.net",number) 
+elif(op==2):
+ generate("https://us-proxy.org",number) 
+elif(op==3):
+ generate("https://free-proxy-list.net/uk-proxy.html",number)
+elif(op==4):
+ generate("https://www.sslproxies.org",number)
+elif(op==5):
+ generate("https://free-proxy-list.net/anonymous-proxy.html",number)
+elif(op==6):
+ socks(number)
+else:
+ print("\033[1;31;40mInvalid input. Quiting...")
+ time.sleep(1.5)
+ sys.exit()
+
+
+
+
+
+
+
